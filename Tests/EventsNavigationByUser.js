efdc3645ba01d55a1616ef8b493cdc3a15b787dc
alilijamal <ali.jamal@strathmore.edu>
2024-08-09 21:51:25 +0300

    some changes

--- Tests/EventsNavigationByUser.js
+++ Tests/EventsNavigationByUser.js
@@ -1,63 +1,20 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-const assert = require('assert')
+const {Builder, By, Key} = require ("selenium-webdriver");
 
-describe('Events In calendar View', function() {
-  this.timeout(30000)
-  let driver
-  let vars
-  beforeEach(async function() {
-    driver = await new Builder().forBrowser('firefox').build()
-    vars = {}
-  })
-  afterEach(async function() {
-    await driver.quit();
-  })
-  it('Events In calendar View', async function() {
-    await driver.get("https://www.ticketvipi.com/?period=upcoming")
-    await driver.manage().window().setRect(1294, 1032)
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    {
-      const element = await driver.findElement(By.css(".hover\\3Atext-rt-white"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
-    }
-    await driver.findElement(By.css(".text-gray-700:nth-child(1)")).click()
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    await driver.findElement(By.css(".w-full:nth-child(3)")).click()
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    {
-      const element = await driver.findElement(By.css(".hover\\3Atext-rt-white"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
-    }
-    await driver.findElement(By.css(".w-full:nth-child(4)")).click()
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    {
-      const element = await driver.findElement(By.css(".hover\\3Atext-rt-white"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
-    }
-    await driver.findElement(By.css(".w-full:nth-child(5)")).click()
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    {
-      const element = await driver.findElement(By.css(".hover\\3Atext-rt-white"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
-    }
-    await driver.findElement(By.css(".w-full:nth-child(6)")).click()
-    await driver.findElement(By.css(".hover\\3Atext-rt-white")).click()
-    await driver.findElement(By.css(".w-full:nth-child(7)")).click()
-  })
-})
+async function navigationbyuser() {
+    
+    let driver = await new Builder().forBrowser("firefox").build();
+    await driver.get("https://www.ticketvipi.com/login")
+
+    //A user Logs in to the site
+    await driver.findElement(By.name("email")).sendKeys("alihayan4@gmail.com");
+    await driver.findElement(By.name("password")).sendKeys("Ali123", Key.RETURN);
+
+    //Navigation through the website
+    //1.Conferences
+
+
+    //AN ERROR OCCURS AT THIS POINT BECAUSE THE BUTTONS WERE NOT DEFINED BY THE PROGRAMMERS. I WILL CONFIRM WITH JOHN AND GET BACK TO THIS
+
+
+}
+navigationbyuser()
--- Tests/UserRegistration.js
+++ Tests/UserRegistration.js
@@ -5,9 +5,11 @@ async function registeruser() {
     let driver = await new Builder().forBrowser("firefox").build();
     await driver.get("https://www.ticketvipi.com/?period=upcoming")
 
+    //click on the button for "CREATE ACCOUNT" on the ticket vipi page
     await driver.findElement(By.className("inline-flex px-4 py-2 text-sm flex-row justify-center items-center rounded-md transform transition-transform duration-200 hover:scale-105 bg-rt-white border border-rt-violet text-rt-violet")).sendKeys(Key.RETURN);
 
 
+    //Add details for registration. NOTE: you cannot run this code twice unless changing info because a user cannot be registered twice
     await driver.findElement(By.name("name")).sendKeys("Shakahola");
     await driver.findElement(By.name("email")).sendKeys("shakaholaVictim@gmail.com");
     await driver.findElement(By.name("phone_number")).sendKeys("0727649898");
