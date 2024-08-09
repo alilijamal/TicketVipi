f85d00368f36dc03c65b01a637a2d6df43393649
alilijamal <ali.jamal@strathmore.edu>
2024-08-09 20:13:19 +0300

    Correction on AdminLogin file

--- Tests/AdminLogIn.js
+++ Tests/AdminLogIn.js
@@ -1,18 +1,12 @@
-const {Builder} = require ("selenium-webdriver");
+const {Builder, By, Key} = require ("selenium-webdriver");
 
 async function adminlogin() {
     
     let driver = await new Builder().forBrowser("firefox").build();
     await driver.get("https://www.ticketvipi.com/login")
 
-    let emailInput = await driver.findElement(By.name('q'));
-    await emailInput.sendKeys('alijamal.abdi04@gmail.com');
-
-    let passwordInput = await driver.findElement(By.name('q'));
-    await passwordInput.sendKeys('ALI123');
-
-    let loginButton = await driver.findElement(By.css('button[type="submit"]'));
-    await loginButton.click();
+    await driver.findElement(By.name("email")).sendKeys("alijamal.abdi04@gmail.com");
+    await driver.findElement(By.name("password")).sendKeys("ALI123", Key.RETURN);
 
 
 
