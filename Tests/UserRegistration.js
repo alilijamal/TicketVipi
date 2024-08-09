dc79f01421ece1aceabc5736cd40d3200abe7674
alilijamal <ali.jamal@strathmore.edu>
2024-08-09 21:12:23 +0300

    Corrected user Registration. NOTE: When running code change the values as user registration will not work with the same values

--- Tests/UserRegistration.js
+++ Tests/UserRegistration.js
@@ -1,31 +1,22 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-const assert = require('assert')
+const {Builder, By, Key} = require ("selenium-webdriver");
 
-describe('User Registration', function() {
-  this.timeout(30000)
-  let driver
-  let vars
-  beforeEach(async function() {
-    driver = await new Builder().forBrowser('firefox').build()
-    vars = {}
-  })
-  afterEach(async function() {
-    await driver.quit();
-  })
-  it('User Registration', async function() {
+async function registeruser() {
+    
+    let driver = await new Builder().forBrowser("firefox").build();
     await driver.get("https://www.ticketvipi.com/?period=upcoming")
-    await driver.manage().window().setRect(1294, 1032)
-    await driver.findElement(By.css(".bg-rt-white > .font-poppins")).click()
-    await driver.findElement(By.name("name")).click()
-    await driver.findElement(By.name("name")).sendKeys("Shimakhokho")
-    await driver.findElement(By.name("email")).click()
-    await driver.findElement(By.name("email")).sendKeys("shimakhokho@gmail.com")
-    await driver.findElement(By.name("phone_number")).click()
-    await driver.findElement(By.name("phone_number")).sendKeys("+254112130190")
-    await driver.findElement(By.name("password")).click()
-    await driver.findElement(By.name("password")).sendKeys("ALI123")
-    await driver.findElement(By.name("password_confirmation")).click()
-    await driver.findElement(By.name("password_confirmation")).sendKeys("ALI123")
-    await driver.findElement(By.css(".bg-gradient-to-r")).click()
-  })
-})
+
+    await driver.findElement(By.className("inline-flex px-4 py-2 text-sm flex-row justify-center items-center rounded-md transform transition-transform duration-200 hover:scale-105 bg-rt-white border border-rt-violet text-rt-violet")).sendKeys(Key.RETURN);
+
+
+    await driver.findElement(By.name("name")).sendKeys("Shakahola");
+    await driver.findElement(By.name("email")).sendKeys("shakaholaVictim@gmail.com");
+    await driver.findElement(By.name("phone_number")).sendKeys("0727649898");
+    await driver.findElement(By.name("password")).sendKeys("ALI123");
+    await driver.findElement(By.name("password_confirmation")).sendKeys("ALI123", Key.RETURN);
+
+
+
+
+
+}
+registeruser()
