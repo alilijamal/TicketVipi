b41a43d17378c46080c167500e139cb8338564f3
alilijamal <ali.jamal@strathmore.edu>
2024-08-09 21:38:40 +0300

    password change correction with comments

--- Tests/AdminPassChange.js
+++ Tests/AdminPassChange.js
@@ -1,19 +1,17 @@
-const {Builder} = require ("selenium-webdriver");
+const {Builder, By, Key} = require ("selenium-webdriver");
 
 async function adminpasschange() {
     
     let driver = await new Builder().forBrowser("firefox").build();
     await driver.get("https://www.ticketvipi.com/login")
 
-    await driver.get("https://www.ticketvipi.com/?period=upcoming")
-    await driver.manage().window().setRect(974, 1032)
-    await driver.findElement(By.css(".bg-gradient-to-r > .font-poppins:nth-child(1)")).click()
-    await driver.findElement(By.name("email")).sendKeys("alijamal.abdi04@gmail.com")
-    await driver.findElement(By.name("password")).sendKeys("ALI123")
-    await driver.findElement(By.css(".forgot-password > .font-poppins")).click()
-    await driver.findElement(By.name("email")).click()
-    await driver.findElement(By.name("email")).sendKeys("alijamal.abdi04@gmail.com")
-    await driver.findElement(By.css(".w-full:nth-child(1) > .font-poppins")).click()
+    //Click reset password
+    await driver.findElement(By.className("font-poppins font-semibold capitalize text-[14px]")).click();
+
+    //Enter Email to proceed
+    await driver.findElement(By.name("email")).sendKeys("alihayan4@gmail.com", Key.RETURN);
+
+    //succeful if it shows the message "An email has been sent to your mailbox"
 
 
 }
